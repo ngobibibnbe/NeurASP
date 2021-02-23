b539b6c299bca7082a4c0c2ae163d593ba27e157
ngobibinbe <anne.ngobibinbe@gmail.com>
2021-02-23 17:44:12 +0100

    ok

--- examples/mnist/deepproblog/generate_data.py
+++ examples/mnist/deepproblog/generate_data.py
@@ -1,3 +1,7 @@
+
+import os
+import sys
+
 import torchvision
 import random
 
---
+++ examples/mnist/deepproblog/test.sh
@@ -0,0 +1,3 @@
+time python generate_data.py >  test_generate.txt
+time python run.py >  test_deepproblog.txt
+time python runb.py  > test_NN.txt
