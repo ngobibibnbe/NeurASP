14b7fd4cc5e7b8ef9d4b1c995741a3769e8f851b
ngobibinbe <anne.ngobibinbe@gmail.com>
2021-02-26 20:00:09 +0100

    nist compraison complete

--- examples/mnist/deepproblog/test.sh
+++ examples/mnist/deepproblog/test.sh
@@ -1,3 +1,5 @@
 time python generate_data.py >  test_generate.txt
-time python run.py >  test_deepproblog.txt
 time python runb.py  > test_NN.txt
+time python run.py > test_deeproblog.txt
+cd ..
+time   python mnist.py > test_NeurASP.txt
