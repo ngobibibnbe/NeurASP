590c426ea35f539dcaf2da1a25e1567e5e8ac03b
ngo.bibinde <anne.ngobibinbe@gmail.com>
2021-03-03 21:37:54 +0100

    'dout'

--- examples/NER & RE/spert/test_conll04.sh
+++ examples/NER & RE/spert/test_conll04.sh
@@ -5,4 +5,5 @@ cd spert
 python automated_evaluation.py "C:\Users\sophie\Desktop\iCS\reseacrch project\NeurASP\NeurASP\examples\NER & RE\CONLL04\logs\spert_treshold_0.4" conll04 > "CONLL04\result_0.4.txt"
 python display_conll04.py
 
+conda deactivate 
 tensorboard --logdir=conll04/threshold
