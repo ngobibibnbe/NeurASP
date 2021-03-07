b9ddb27b1989ad97679ed4cb653324ac4b1a05c0
ngobibinbe <anne.ngobibinbe@gmail.com>
2021-03-08 00:03:48 +0100

    env ok

--- examples/NER&RE/spert/test_conll04.sh
+++ examples/NER&RE/spert/test_conll04.sh
@@ -1,9 +1,13 @@
-
+conda activate NeurASP2
 cd ..
 python test_conll04.py 
 cd spert
-python automated_evaluation.py "C:\Users\sophie\Desktop\iCS\reseacrch project\NeurASP\NeurASP\examples\NER & RE\CONLL04\logs\spert_treshold_0.4" conll04 > "CONLL04\result_0.4.txt"
-python display_conll04.py
+python automated_evaluation.py "../CONLL04/logs/spert_treshold_0.4" conll04 > "CONLL04/result_0.4.txt"
+python automated_evaluation.py "../CONLL04/logs/spert_treshold_0.1" conll04 > "CONLL04/result_0.1.txt"
+python automated_evaluation.py "../CONLL04/logs/spert_treshold_0.8" conll04 > "CONLL04/result_0.8.txt"
+
 
 conda deactivate 
-tensorboard --logdir=conll04/threshold
+rm -r CONLL04/Threshold/*
+python display_conll04.py
+tensorboard --logdir=CONLL04/Threshold
